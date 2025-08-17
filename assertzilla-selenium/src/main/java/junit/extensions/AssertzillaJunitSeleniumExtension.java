aa1c49d637cd8d28cd495529aff2402c97ecfc5b
Maciej Fec <maciejfec1996@gmail.com>
2025-08-17 11:01:27 +0100

    feat: new module

---
+++ assertzilla-selenium/src/main/java/junit/extensions/AssertzillaJunitSeleniumExtension.java
@@ -0,0 +1,12 @@
+package junit.extensions;
+
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+class AssertzillaJunitSeleniumExtension implements BeforeEachCallback {
+
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
+
+    }
+}
---
+++ assertzilla-selenium/src/main/java/junit/extensions/AssertzillaSeleniumTest.java
@@ -0,0 +1,14 @@
+package junit.extensions;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ExtendWith(AssertzillaJunitSeleniumExtension.class)
+public @interface AssertzillaSeleniumTest {
+}
