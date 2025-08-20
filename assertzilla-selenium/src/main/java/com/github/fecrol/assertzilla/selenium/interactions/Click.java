5e7c2df2ac03b3d922399c26612251c92fa06044
Maciej Fec <maciejfec1996@gmail.com>
2025-08-20 21:07:09 +0100

    feat: initial commit

---
+++ assertzilla-selenium/src/main/java/com/github/fecrol/assertzilla/selenium/interactions/Click.java
@@ -0,0 +1,24 @@
+package com.github.fecrol.assertzilla.selenium.interactions;
+
+import com.github.fecrol.assertzilla.core.annotations.Log;
+import com.github.fecrol.assertzilla.core.interactions.Interaction;
+import com.github.fecrol.assertzilla.selenium.ui.components.WebElement;
+
+public class Click implements Interaction {
+
+    private WebElement webElement;
+
+    private Click(WebElement webElement) {
+        this.webElement = webElement;
+    }
+
+    public static Click on(WebElement webElement) {
+        return new Click(webElement);
+    }
+
+    @Override
+    @Log("Click on ${webElement}")
+    public void perform() {
+        webElement.resolve().click();
+    }
+}
