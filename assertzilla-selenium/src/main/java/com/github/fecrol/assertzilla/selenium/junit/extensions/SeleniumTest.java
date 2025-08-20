e0ce6e58d124342326092a9206b36cf773d322d4
Maciej Fec <maciejfec1996@gmail.com>
2025-08-20 20:55:42 +0100

    feat: rename

--- assertzilla-selenium/src/main/java/com/github/fecrol/assertzilla/selenium/junit/extensions/SeleniumTest.java
+++ assertzilla-selenium/src/main/java/com/github/fecrol/assertzilla/selenium/junit/extensions/SeleniumTest.java
@@ -10,5 +10,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @ExtendWith(AssertzillaJunitSeleniumExtension.class)
-public @interface AssertzillaSeleniumTest {
+public @interface SeleniumTest {
 }
