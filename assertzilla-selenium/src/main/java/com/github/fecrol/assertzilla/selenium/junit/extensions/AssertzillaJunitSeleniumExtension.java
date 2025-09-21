98f866a4d00b8883f5d233ed252f27ca15f35a92
Maciej Fec <maciejfec1996@gmail.com>
2025-09-21 18:03:06 +0100

    feat: add before all callback to extension to load in webdriver configuration before all tests

--- assertzilla-selenium/src/main/java/com/github/fecrol/assertzilla/selenium/junit/extensions/AssertzillaJunitSeleniumExtension.java
+++ assertzilla-selenium/src/main/java/com/github/fecrol/assertzilla/selenium/junit/extensions/AssertzillaJunitSeleniumExtension.java
@@ -2,16 +2,21 @@ package com.github.fecrol.assertzilla.selenium.junit.extensions;
 
 import com.github.fecrol.assertzilla.selenium.webdriver.AssertzillaWebDriverManager;
 import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-class AssertzillaJunitSeleniumExtension implements BeforeEachCallback, AfterEachCallback {
+class AssertzillaJunitSeleniumExtension implements BeforeAllCallback, BeforeEachCallback, AfterEachCallback {
+
+    @Override
+    public void beforeAll(ExtensionContext context) throws Exception {
+        AssertzillaWebDriverManager.loadWebDriverConfiguration();
+    }
 
     @Override
     public void beforeEach(ExtensionContext context) throws Exception {
         AssertzillaWebDriverManager.resetDriver();
-        AssertzillaWebDriverManager.initialiseWebDriver(new ChromeDriver());
+        AssertzillaWebDriverManager.initialiseWebDriver();
     }
 
     @Override
