8346c2f3dd44fa535012ec273fa32c9926a4a6b9
Maciej Fec <maciejfec1996@gmail.com>
2025-09-27 21:24:32 +0100

    feat: add logic to load yaml as pojo from specific node in the yml file

--- assertzilla-core/src/main/java/com/github/fecrol/assertzilla/core/ConfigurationLoader.java
+++ assertzilla-core/src/main/java/com/github/fecrol/assertzilla/core/ConfigurationLoader.java
@@ -1,17 +1,21 @@
 package com.github.fecrol.assertzilla.core;
 
 import com.github.fecrol.assertzilla.core.exceptions.ConfigurationIOException;
+import com.google.common.base.Strings;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.InputStream;
+import java.util.Map;
 
 public class ConfigurationLoader {
 
     private Yaml YAML;
+    private String node;
     private String configurationFile;
 
     private ConfigurationLoader(String configurationFile) {
         YAML = new Yaml();
+        this.node = null;
         this.configurationFile = configurationFile;
     }
 
@@ -19,12 +23,21 @@ public class ConfigurationLoader {
         return new ConfigurationLoader(configurationName);
     }
 
+    public ConfigurationLoader node(String node) {
+        this.node = node;
+        return this;
+    }
+
     public <T extends Configuration> T as(Class<T> instance) {
         String configurationPath = String.format("assertzilla/%s", configurationFile);
         ClassLoader classLoader = ConfigurationLoader.class.getClassLoader();
 
         try (InputStream inputStream = classLoader.getResourceAsStream(configurationPath)) {
-            return YAML.loadAs(inputStream, instance);
+            if (!Strings.isNullOrEmpty(node)) {
+                Map<String, Object> root = YAML.load(inputStream);
+                Object webDriverNode = root.get(node);
+                return YAML.loadAs(YAML.dump(webDriverNode), instance);
+            } else return YAML.loadAs(inputStream, instance);
         } catch (Exception e) {
             throw new ConfigurationIOException(e);
         }
